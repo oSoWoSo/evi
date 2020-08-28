fdc228b695186b26a864c36883c3cbabb67312a5
zenobit <kvvark@gmail.com>
2020-08-28 01:25:21 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,23 +2,57 @@
 # Void Linux post-install script
 echo "$(tput setaf 1)						Starting Void Linux post-install script$(tput sgr 0)"
 
+# Prerequisities-----------------------------------------------------------------------------------------
+# Freshly installed base system
+# Script will inastall rest..
+# git
+# And clone this repository
+sudo xbps-install -Sy git
+mkdir ~/bin
+git clone https://gitlab.com/awesome-void/install ~/bin/install
+cd ~/bin/install
+
 # Install packages-----------------------------------------------------------------------------------------
-sudo xbps-install -Su
-sudo xbps-install -Sy $(cat INSTALL/1_repos)
+# Nonfree and multilib repos
+sudo xbps-install -y $(cat INSTALL/1_repos)
 sudo xbps-install -Sy $(cat INSTALL/2_base)
 
-git config --global credential.helper store
+# Remember git login information?
+#git config --global credential.helper store
+# Clone also personal dotfiles from gitlab? 
 #git clone https://gitlab.com/awesome-void/awesomeVoid ~/bin/dotfiles
 
+# Choose cpu-----------------------------------------------------------------------------------------
+echo "$(tput setaf 1)						Which CPU do you use?$(tput sgr 0)"
+read -p "AMD (a) or INTEL (i)     [a/i] " -n 1 cpu
+echo
+    if [[ $cpu = "a" ]]; then
+        sudo xbps-install -y $(cat INSTALL/3_nvidia)
+        sudo nvidia-xconfig
+        
+        read -p "Do you want to install PCI passthrough? NO (n) or YES (y)      [n/y] " -n 1 pass
+        echo
+            if [[ $pass = "y" ]]; then
+                if [[ $cpu = "a" ]]; then
+                    modprobe kvm-amd
+                if [[ $cpu = "i" ]]; then
+                    modprobe kvm-intel
+                fi
+                then
+                    sudo cp -r /OVMF/ /usr/share/ovmf
+                    chmod +x /INSTALL/pass.sh
+                    sudo ./INSTALL/pass.sh
+            fi
+
 # Choose gpu-----------------------------------------------------------------------------------------
-echo "$(tput setaf 1)						Which GPU you using?$(tput sgr 0)"
+echo "$(tput setaf 1)						Which GPU do you use?$(tput sgr 0)"
 read -p "NVIDIA (n), AMD (a), INTEL (i) or QEMU (q)      [n/a/i/q] " -n 1 video
 echo
     if [[ $video = "n" ]]; then
-        sudo xbps-install -Sy $(cat INSTALL/3_nvidia)
+        sudo xbps-install -y $(cat INSTALL/3_nvidia)
         sudo nvidia-xconfig
         
-        read -p "Want install PCI passthrough? NO (n) or YES (y)      [n/y] " -n 1 pass
+        read -p "Do you want to install PCI passthrough? NO (n) or YES (y)      [n/y] " -n 1 pass
         echo
             if [[ $pass = "y" ]]; then
             modprobe kvm-amd
@@ -27,23 +61,23 @@ echo
             sudo ./INSTALL/pass.sh
         fi
     elif [[ $video = "a" ]]; then
-        sudo xbps-install -Sy $(cat INSTALL/3_ati)
+        sudo xbps-install -y $(cat INSTALL/3_ati)
         modprobe kvm-amd
 
     elif [[ $video = "i" ]]; then
-        sudo xbps-install -Sy $(cat INSTALL/3_intel)
+        sudo xbps-install -y $(cat INSTALL/3_intel)
 
     elif [[ $video = "q" ]]; then
-        sudo xbps-install -Sy $(cat INSTALL/3_qemu)
+        sudo xbps-install -y $(cat INSTALL/3_qemu)
 fi
-sudo xbps-install -Sy $(cat INSTALL/4_desktop)
 
 # Choose window manager-----------------------------------------------------------------------------------------
-echo "$(tput setaf 1)						Which windows manager you want use?$(tput sgr 0)"
+echo "$(tput setaf 1)						Which window manager do you want to use?$(tput sgr 0)"
 read -p "OPENBOX (o) or AWESOME (a) or SOMETHING ELSE (s)          [o/a/s] " -n 1 vm
 echo
     if [[ $vm = "o" ]]; then
-        sudo xbps-install -Sy $(cat INSTALL/5_openbox)
+        sudo xbps-install -y $(cat INSTALL/4_desktop)
+        sudo xbps-install -y $(cat INSTALL/5_openbox)
         sudo -u $USER obmenu-generator -p -i -u -d -c
 echo
 "autorandr common &
@@ -54,7 +88,8 @@ volumeicon &
 conky &" >> ~/.config/openbox/autostart
 #        cp ~/bin/dotfiles/home/zen/.config/openbox/rc.xml ~/.config/openbox
     elif [[ $vm = "a" ]]; then
-        sudo xbps-install -Sy $(cat INSTALL/5_awesome)
+        sudo xbps-install -y $(cat INSTALL/4_desktop)
+        sudo xbps-install -y $(cat INSTALL/5_awesome)
 
     elif [[ $vm = "s" ]]; then
         echo "$(tput setaf 1) Install something else after..$(tput sgr 0)"
@@ -65,18 +100,18 @@ fi
 #sudo xbps-install -Sy $(cat INSTALL/8_big)
 
 # printer support-----------------------------------------------------------------------------------------
-echo "$(tput setaf 1)						Want install printer support?$(tput sgr 0)"
+echo "$(tput setaf 1)						Do you want to install printer support?$(tput sgr 0)"
 read -p "NO (n) or YES (y)      [n/y] " -n 1 cups
 echo
     if [[ $cups = "y" ]]; then
-        sudo xbps-install -Sy $(cat INSTALL/9_print)
+        sudo xbps-install -y $(cat INSTALL/9_print)
         sudo ln -s /etc/sv/cupsd /var/service
     fi    
 
 #sudo xbps-install -Sy $(cat INSTALL/)
 
 # make fish base shell-----------------------------------------------------------------------------------------
-sudo usermod --shell /bin/fish zen
+sudo usermod --shell /bin/fish $USER
 #echo ". ~/.config/fish/aliases.fish" >> ~/.config/fish/config.fish
 #echo "alias xterm 'sakura'" >> ~/.config/fish/aliases.fish
 
@@ -93,11 +128,11 @@ cd ~/.void
 git init --bare
 
 # Share packages with void devs-----------------------------------------------------------------------------------------
-echo "$(tput setaf 1)						Want share packages with void devs?$(tput sgr 0)"
+echo "$(tput setaf 1)						Do you want to share package statistics with void devs?$(tput sgr 0)"
 read -p "NO (n) or YES (y)      [n/y] " -n 1 pop
 echo
     if [[ $pop = "y" ]]; then
-        sudo xbps-install -Sy popcorn
+        sudo xbps-install -y popcorn
         sudo ln -s /etc/sv/popcorn /var/service
     fi 
 
@@ -110,7 +145,7 @@ sudo ln -s /etc/sv/nanoklogd /var/service
 sudo ln -s /etc/sv/crond /var/service
 sudo ln -s /etc/sv/chronyd /var/service
 
-echo "$(tput setaf 1)						Want run lightdm now?$(tput sgr 0)"
+echo "$(tput setaf 1)						Do you want to run lightdm now?$(tput sgr 0)"
 read -p "Run lightdm now? YES (y) or NO (n)?          [y/n] " -n 1 lightdm
 echo
     if [[ $lightdm = "y" ]]; then
