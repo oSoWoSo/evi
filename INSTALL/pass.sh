966a66b9df52e03998fdb3eb6fb9debe0e6d9f16
zenobit <kvvark@gmail.com>
2020-08-28 07:46:31 +0000

    Update INSTALL/pass.sh, install.sh files

--- INSTALL/pass.sh
+++ INSTALL/pass.sh
@@ -1,12 +1,12 @@
 echo "$(tput setaf 1)						Installing VGA passthrough$(tput sgr 0)"
-echo "Edit grub: intel_iommu=on or amd_iommu=on rd.driver.pre=vfio-pci kvm.ignore_msrs=1" >> /etc/default/grub
+echo "#Edit grub: intel_iommu=on OR amd_iommu=on rd.driver.pre=vfio-pci kvm.ignore_msrs=1" >> /etc/default/grub
 micro /etc/default/grub
 echo "Updating grub"
 grub-mkconfig -o /boot/grub/grub.cfg
 echo "Getting GPU passthrough scripts ready"
-cp /INSTALL/vfio-pci-override-vga.sh /usr/bin/vfio-pci-override-vga.sh
+cp INSTALL/vfio-pci-override-vga.sh /usr/bin/vfio-pci-override-vga.sh
 chmod 755 /usr/bin/vfio-pci-override-vga.sh
 echo "install vfio-pci /usr/bin/vfio-pci-override-vga.sh" > /etc/modprobe.d/local.conf
-cp /INSTALL/local.conf /etc/dracut.conf.d/local.conf
+cp INSTALL/local.conf /etc/dracut.conf.d/local.conf
 echo "Generating initramfs"
 dracut -f --kver $(uname -r)
--- install.sh
+++ install.sh
@@ -1,12 +1,36 @@
 #!/bin/bash
 # Void Linux post-install script
-echo "$(tput setaf 1)						Starting Void Linux post-install script$(tput sgr 0)"
+echo "$(tput setaf 3)Starting Void Linux post-install script$(tput sgr 0)"
+
+# Ask part
+echo "$(tput setaf 1)Which CPU do you use?$(tput sgr 0)"
+read -p "AMD (a) or INTEL (i)	[a/i] " -n 1 cpu
+echo
+echo "$(tput setaf 1)Which GPU do you use?$(tput sgr 0)"
+read -p "NVIDIA (n), AMD (a), INTEL (i) or QEMU (q)	[n/a/i/q] " -n 1 video
+echo
+echo "$(tput setaf 1)Do you want to install PCI passthrough?$(tput sgr 0)"
+read -p "NO (n) or YES (y)	[n/y] " -n 1 pass
+echo
+echo "$(tput setaf 1)Which window manager do you want to use?$(tput sgr 0)"
+read -p "OPENBOX (o) or AWESOME (a) or SOMETHING ELSE (s)	[o/a/s] " -n 1 vm
+echo
+echo "$(tput setaf 1)Do you want to install printer support?$(tput sgr 0)"
+read -p "NO (n) or YES (y)	[n/y] " -n 1 cups
+echo
+echo "$(tput setaf 1)Do you want to share package statistics with void devs?$(tput sgr 0)"
+read -p "NO (n) or YES (y)	[n/y] " -n 1 pop
+echo
+echo "$(tput setaf 1)Unncomment desired language(s) $(tput sgr 0)"
+sleep 3
+sudo micro /etc/default/libc-locales
 
 # Prerequisities-----------------------------------------------------------------------------------------
 # Freshly installed base system
 # Script will inastall rest..
 # git
 # And clone this repository
+
 sudo xbps-install -Sy git
 mkdir ~/bin
 git clone https://gitlab.com/awesome-void/install ~/bin/install
@@ -23,91 +47,127 @@ sudo xbps-install -Sy $(cat INSTALL/2_base)
 #git clone https://gitlab.com/awesome-void/awesomeVoid ~/bin/dotfiles
 
 # Choose cpu-----------------------------------------------------------------------------------------
-#echo "$(tput setaf 1)						Which CPU do you use?$(tput sgr 0)"
-#read -p "AMD (a) or INTEL (i)     [a/i] " -n 1 cpu
-#echo
-#    if [[ $cpu = "a" ]]; then
-#        sudo xbps-install -y $(cat INSTALL/3_nvidia)
-#        sudo nvidia-xconfig
-#    elif [[ $cpu = "i" ]]; then    
-#        read -p "Do you want to install PCI passthrough? NO (n) or YES (y)      [n/y] " -n 1 pass
-#        echo
-#            if [[ $pass = "y" ]]; then
-#                if [[ $cpu = "a" ]]; then
-#                    modprobe kvm-amd
-#                if [[ $cpu = "i" ]]; then
-#                    modprobe kvm-intel
-#                fi
-#                    sudo cp -r /OVMF/ /usr/share/ovmf
-#                    chmod +x /INSTALL/pass.sh
-#                    sudo ./INSTALL/pass.sh
-#            fi
-#
-# Choose gpu-----------------------------------------------------------------------------------------
-echo "$(tput setaf 1)						Which GPU do you use?$(tput sgr 0)"
-read -p "NVIDIA (n), AMD (a), INTEL (i) or QEMU (q)      [n/a/i/q] " -n 1 video
-echo
-    if [[ $video = "n" ]]; then
-        sudo xbps-install -y $(cat INSTALL/3_nvidia)
+    if [[ $cpu = "a" ]]; then
+		echo amd
+		if [[ $video = "n" ]]; then
+		echo amd nvidia
+		sudo xbps-install -y $(cat INSTALL/3_nvidia)
         sudo nvidia-xconfig
-        
-        read -p "Do you want to install PCI passthrough? NO (n) or YES (y)      [n/y] " -n 1 pass
-        echo
-            if [[ $pass = "y" ]]; then
-            modprobe kvm-amd
-            sudo cp -r /OVMF/ /usr/share/ovmf
-            chmod +x /INSTALL/pass.sh
-            sudo ./INSTALL/pass.sh
-        fi
-    elif [[ $video = "a" ]]; then
-        sudo xbps-install -y $(cat INSTALL/3_ati)
-        modprobe kvm-amd
-
-    elif [[ $video = "i" ]]; then
-        sudo xbps-install -y $(cat INSTALL/3_intel)
-
-    elif [[ $video = "q" ]]; then
-        sudo xbps-install -y $(cat INSTALL/3_qemu)
+			if [[ $pass = "y" ]]; then
+			echo amd nvidia yes
+			modprobe kvm-amd
+			sudo cp -r OVMF /usr/share/ovmf
+			chmod +x INSTALL/pass.sh
+			sudo ./INSTALL/pass.sh
+			fi
+		elif [[ $video = "a" ]]; then	
+			echo amd amd
+			if [[ $pass = "y" ]]; then
+			echo amd amd yes
+			modprobe kvm-amd
+			sudo cp -r OVMF /usr/share/ovmf
+			chmod +x INSTALL/pass.sh
+			sudo ./INSTALL/pass.sh
+			fi
+        elif [[ $video = "i" ]]; then	
+			echo amd intel
+			if [[ $pass = "y" ]]; then
+			echo amd intel yes
+			modprobe kvm-amd
+			sudo cp -r OVMF /usr/share/ovmf
+			chmod +x INSTALL/pass.sh
+			sudo ./INSTALL/pass.sh
+			fi
+		elif [[ $video = "q" ]]; then
+			echo amd qemu
+			sudo xbps-install -y $(cat INSTALL/3_qemu)
+		  fi
+		     
+    elif [[ $cpu = "i" ]]; then
+		echo intel
+		if [[ $video = "n" ]]; then
+		echo intel nvidia
+		sudo xbps-install -y $(cat INSTALL/3_nvidia)
+        sudo nvidia-xconfig
+			if [[ $pass = "y" ]]; then
+			echo intel nvidia yes
+			modprobe kvm-intel
+			sudo cp -r OVMF /usr/share/ovmf
+			chmod +x INSTALL/pass.sh
+			sudo ./INSTALL/pass.sh
+			fi
+		elif [[ $video = "a" ]]; then	
+			echo intel amd
+			if [[ $pass = "y" ]]; then
+			echo intel amd yes
+			modprobe kvm-intel
+			sudo cp -r OVMF /usr/share/ovmf
+			chmod +x INSTALL/pass.sh
+			sudo ./INSTALL/pass.sh
+			fi
+        elif [[ $video = "i" ]]; then	
+			echo intel intel
+			if [[ $pass = "y" ]]; then
+			echo intel intel yes
+			modprobe kvm-intel
+			sudo cp -r OVMF /usr/share/ovmf
+			chmod +x INSTALL/pass.sh
+			sudo ./INSTALL/pass.sh
+			fi
+		elif [[ $video = "q" ]]; then
+			echo intel qemu
+#			sudo xbps-install -y $(cat INSTALL/3_qemu)
+		  fi
 fi
 
+# Choose gpu-----------------------------------------------------------------------------------------
+#    if [[ $video = "n" ]]; then
+#        sudo xbps-install -y $(cat INSTALL/3_nvidia)
+#        sudo nvidia-xconfig
+#            if [[ $pass = "y" ]]; then
+#            modprobe kvm-amd
+#            sudo cp -r /OVMF /usr/share/ovmf
+#            chmod +x /INSTALL/pass.sh
+#            sudo ./INSTALL/pass.sh
+#        fi
+#    elif [[ $video = "a" ]]; then
+#        sudo xbps-install -y $(cat INSTALL/3_ati)
+#        modprobe kvm-amd
+#    elif [[ $video = "i" ]]; then
+#        sudo xbps-install -y $(cat INSTALL/3_intel)
+#
+#    elif [[ $video = "q" ]]; then
+#        sudo xbps-install -y $(cat INSTALL/3_qemu)
+#fi
+
 # Choose window manager-----------------------------------------------------------------------------------------
-echo "$(tput setaf 1)						Which window manager do you want to use?$(tput sgr 0)"
-read -p "OPENBOX (o) or AWESOME (a) or SOMETHING ELSE (s)          [o/a/s] " -n 1 vm
-echo
     if [[ $vm = "o" ]]; then
         sudo xbps-install -y $(cat INSTALL/4_desktop)
         sudo xbps-install -y $(cat INSTALL/5_openbox)
         sudo -u $USER obmenu-generator -p -i -u -d -c
-echo
-"autorandr common &
-tint2 &
-setxkbmap cz &
-nitrogen --restore &
-volumeicon &
-conky &" >> ~/.config/openbox/autostart
+		echo "autorandr common &
+		tint2 &
+		setxkbmap cz &
+		nitrogen --restore &
+		volumeicon &
+		conky &" >> ~/.config/openbox/autostart
 #        cp ~/bin/dotfiles/home/zen/.config/openbox/rc.xml ~/.config/openbox
     elif [[ $vm = "a" ]]; then
+		echo awesome
         sudo xbps-install -y $(cat INSTALL/4_desktop)
         sudo xbps-install -y $(cat INSTALL/5_awesome)
+	  fi
 
-    elif [[ $vm = "s" ]]; then
-        echo "$(tput setaf 1) Install something else after..$(tput sgr 0)"
-fi
-
-#sudo xbps-install -Sy $(cat INSTALL/6_media)
+sudo xbps-install -Sy $(cat INSTALL/6_media)
 #sudo xbps-install -Sy $(cat INSTALL/7_virtual)
 #sudo xbps-install -Sy $(cat INSTALL/8_big)
+#sudo xbps-install -Sy $(cat INSTALL/)
 
 # printer support-----------------------------------------------------------------------------------------
-echo "$(tput setaf 1)						Do you want to install printer support?$(tput sgr 0)"
-read -p "NO (n) or YES (y)      [n/y] " -n 1 cups
-echo
     if [[ $cups = "y" ]]; then
         sudo xbps-install -y $(cat INSTALL/9_print)
         sudo ln -s /etc/sv/cupsd /var/service
     fi    
-
-#sudo xbps-install -Sy $(cat INSTALL/)
 
 # make fish base shell-----------------------------------------------------------------------------------------
 sudo usermod --shell /bin/fish $USER
@@ -118,7 +178,6 @@ sudo usermod --shell /bin/fish $USER
 sudo usermod -a -G socklog $USER
 
 # čeština-----------------------------------------------------------------------------------------
-sudo micro /etc/default/libc-locales
 sudo xbps-reconfigure -f glibc-locales
 
 # creating bare repository-----------------------------------------------------------------------------------------
@@ -127,30 +186,25 @@ cd ~/.void
 git init --bare
 
 # Share packages with void devs-----------------------------------------------------------------------------------------
-echo "$(tput setaf 1)						Do you want to share package statistics with void devs?$(tput sgr 0)"
-read -p "NO (n) or YES (y)      [n/y] " -n 1 pop
-echo
     if [[ $pop = "y" ]]; then
         sudo xbps-install -y PopCorn
-        sudo ln -s /etc/sv/popcorn /var/service
+        sudo ln -s /etc/sv/popcorn /var/service/
     fi 
 
 # Install services-----------------------------------------------------------------------------------------
-sudo ln -s /etc/sv/dbus /var/service
-sudo ln -s /etc/sv/elogind /var/service
-#sudo ln -s /etc/sv/acpid /var/service
-sudo ln -s /etc/sv/socklog-unix /var/service
-sudo ln -s /etc/sv/nanoklogd /var/service
-sudo ln -s /etc/sv/crond /var/service
-sudo ln -s /etc/sv/chronyd /var/service
+sudo ln -s /etc/sv/dbus /var/service/
+sudo ln -s /etc/sv/elogind /var/service/
+#sudo ln -s /etc/sv/acpid /var/service/
+sudo ln -s /etc/sv/socklog-unix /var/service/
+sudo ln -s /etc/sv/nanoklogd /var/service/
+sudo ln -s /etc/sv/crond /var/service/
+sudo ln -s /etc/sv/chronyd /var/service/
 
 echo "$(tput setaf 1)						Do you want to run lightdm now?$(tput sgr 0)"
 read -p "Run lightdm now? YES (y) or NO (n)?          [y/n] " -n 1 lightdm
 echo
     if [[ $lightdm = "y" ]]; then
-        sudo ln -s /etc/sv/lightdm /var/service
+        sudo ln -s /etc/sv/lightdm /var/service/
         echo "$(tput setaf 1)						Enjoy void linux$(tput sgr 0)"
-    elif [[ $lightdm = "n" ]]; then
-    sudo ln -s /etc/sv/lightdm /etc/runit/runsvdir/default/
-    echo "$(tput setaf 1)						Enjoy void linux$(tput sgr 0)"
 fi
+#sudo restart
